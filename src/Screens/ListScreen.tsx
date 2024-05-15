227b709fe0f27bebf49f8084dbd05bba2797b7e9
MelULaval <melissa.raymond.4@ulaval.ca>
2024-05-15 14:27:48 -0400

    style: colors file added

--- src/Screens/ListScreen.tsx
+++ src/Screens/ListScreen.tsx
@@ -1,11 +1,21 @@
-import {Button, Text, View} from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
 import { ListScreenNavigationProp } from '../main/navigation/Navigator';
+import colors from '../main/styles/colors';
 
 export default function ListScreen({navigation}: ListScreenNavigationProp) {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>ListScreen</Text>
       <Button title=" go Back " onPress={()=> navigation.goBack()}/>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: colors.background,
+    paddingLeft:16,
+    paddingRight: 16,
+  }
+})
---
+++ src/main/styles/colors.ts
@@ -0,0 +1,14 @@
+
+const colors = {
+    background:'#FFFCF5',
+    primary: '#D0A2A2',
+    secondary: '#ECC7C6',
+    textLight: '#F3EBE9',
+    textDark: '#726C76',
+    accent: '#BDCBB2',
+    error: '#',
+    success: '#',
+    neutral: '#D9D6CF'
+
+}
+export default colors;
