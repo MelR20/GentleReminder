8b3e3067d876723ab0d49c2fbc795da0326f4839
MelULaval <melissa.raymond.4@ulaval.ca>
2024-08-14 13:59:06 -0400

    feat- Basic affichage collections in HomePage

--- src/Screens/HomeScreen.tsx
+++ src/Screens/HomeScreen.tsx
@@ -7,20 +7,25 @@ import colors from '../main/styles/colors';
 import Title from '../main/component/Title';
 import spacing from '../main/styles/spacing';
 import ExpandableMenu from '../main/component/ExpandableMenu';
+import { useContext, useId } from 'react';
+import {ListContext} from '../main/Contexts/ListContext';
 
 export default function Home({navigation}: HomeScreenNavigationProp) {
+  const {collections, setCollections} = useContext(ListContext);
+
   return (
     <View style={styles.container}>
       <View style={styles.titleContainer}>
         <Title label="Gentle Reminder" />
         <ExpandableMenu />
       </View>
-      {/* <Button
-        title="go to list"
-        onPress={() => {
-          navigation.navigate('List', {id: 'test'});
-        }}
-      /> */}
+      
+      {collections.map((collection)=>(
+        <View>
+          <Text>{collection.label}</Text>
+          {collection.reminders.map((reminder)=>(<Text>{reminder.label}</Text>))}
+        </View>
+      ))}
       
     </View>
   );
