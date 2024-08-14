b4ba39e2bbeb935576532aa8ae0ee09df2272e75
MelULaval <melissa.raymond.4@ulaval.ca>
2024-08-14 12:27:34 -0400

    fix- ExpandableMenbu on top of everything

--- src/Screens/HomeScreen.tsx
+++ src/Screens/HomeScreen.tsx
@@ -7,21 +7,30 @@ import colors from '../main/styles/colors';
 import Title from '../main/component/Title';
 import spacing from '../main/styles/spacing';
 import ExpandableMenu from '../main/component/ExpandableMenu';
+import {useContext} from 'react';
+import {ListContext} from '../main/Contexts/ListContext';
 
 export default function Home({navigation}: HomeScreenNavigationProp) {
+  const {collections, setCollections} = useContext(ListContext);
+
   return (
     <View style={styles.container}>
       <View style={styles.titleContainer}>
         <Title label="Gentle Reminder" />
         <ExpandableMenu />
       </View>
-      {/* <Button
-        title="go to list"
-        onPress={() => {
-          navigation.navigate('List', {id: 'test'});
-        }}
-      /> */}
-      
+      <View>
+        {collections.map(collection => (
+          <View>
+            <Text>{collection.label}</Text>
+            <View style={{marginLeft: 8}}>
+              {collection.reminders.map(reminder => (
+                <Text>{reminder.label}</Text>
+              ))}
+            </View>
+          </View>
+        ))}
+      </View>
     </View>
   );
 }
@@ -32,9 +41,9 @@ const styles = StyleSheet.create({
     backgroundColor: colors.background,
     paddingLeft: spacing.xs,
     paddingRight: spacing.xs,
-
   },
   titleContainer: {
+    zIndex: 9999,
     paddingTop: spacing.lg,
     margin: spacing.sm,
     flexDirection: 'row',
